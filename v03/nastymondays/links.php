489ca983901dc0c81ff7763389fb4d615c188b82
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-06-21 17:17:11 +0200

    init commit

---
+++ v03/nastymondays/links.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+	* Template Name: Publicidad
+	*
+	 * @package WordPress
+	 * @subpackage nastymondays
+ */
+get_header(); ?>
+
+<h2>Sponsorship:</h2>
+	<ul class="promotores">
+		<?php wp_get_archives(); ?>
+	</ul>
+</div>
+
+<?php get_footer(); ?>
