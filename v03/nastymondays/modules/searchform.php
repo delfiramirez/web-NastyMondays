44ed358865fd79064c0440a2c4a1e4fef3579f75
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-06-21 17:06:26 +0200

    init commit

---
+++ v03/nastymondays/modules/searchform.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	* Custom Module SearchForm
+	*
+	 * @package WordPress
+	 * @subpackage nastymondays
+ */
+ ?>
+	
+	<form 
+	method="get"
+	id="searchform" 
+	class="searchform" 
+	action="<?php bloginfo('url'); ?>" />
+				<div>
+					<input 
+					type="text" 
+					value="<?php the_search_query(); ?>" 
+					name="s"
+					id="s" />
+					<input 
+					type="submit"
+					id="searchsubmit" 
+					value="Search" />
+				</div>
+	</form>
---
+++ v03/nastymondays/site-map.php
@@ -0,0 +1,25 @@
+﻿<?php
+/*
+	*Template Name: Site-Map
+	*
+	 * @package WordPress
+	 * @subpackage nastymondays
+ */
+ php get_header(); ?>
+		<h2 class="nasty2">Nasty Mondays Site Map</h2>
+			<div class="events">
+				<h3>Sections</h3>
+				</div>
+				<?php echo hierarchical_submenu($post); ?>
+				<h3>Events</h3>
+				<?php bm_displayArchives(); ?>
+				<hr />
+				<?php wp_nav_menu('menu=NastyMondays'); ?>
+				<hr />
+				<?php get_sidebar(); ?>     
+				<?php get_template_part( 'segonquart' );  ?>
+				<?php get_footer(); ?>	
+			</div>
+		</div>
+	</body>
+</html>
