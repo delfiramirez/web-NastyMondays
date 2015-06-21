c707ca70094dc12719355791442fd3dac2674372
delfi <delfi@segonquart.localhost>
2015-06-21 22:52:32 +0200

    refactor Signed-off-by:delfi <delfi@segonquart.localhost>

--- v03/nastymondays/site-map.php
+++ v03/nastymondays/site-map.php
@@ -5,26 +5,31 @@
  * @package WordPress
  * @subpackage nastymondays
  */
-get_header();
+get_header ();
 ?>
 
-<h2 class="nasty2">Nasty Mondays Site Map</h2>
+<h2 class="nasty2">
+    <?php echo $blogName; ?> Site Map
+</h2>
+
 <div class="events">
-    <h3>Sections</h3>
+    <h3>
+        <?php print'Sections'; ?>
+    </h3>
 </div>
-<?php echo hierarchical_submenu($post); ?>
-<h3>Events</h3>
-<?php bm_displayArchives(); ?>
+<?php echo hierarchical_submenu ($post); ?>
+<h3>
+    <?php print'Events'; ?>
+</h3>
+
+<?php bm_displayArchives (); ?>
 <hr />
-<?php wp_nav_menu('menu=NastyMondays'); ?>
+<?php wp_nav_menu ('menu=NastyMondays'); ?>
 <hr />
-<?php get_sidebar(); ?>
-<?php get_template_part('segonquart'); ?>
-<?php get_footer(); ?>
+<?php get_sidebar (); ?>
+<?php get_template_part ('segonquart'); ?>
+<?php get_footer (); ?>
 </div>
 </div>
 </body>
 </html>
-
-<?php
-?>
