6ad2883b4d5f12f91cc4a98c9badbdeed32f477a
delfi <delfi@segonquart.localhost>
2015-06-21 22:26:21 +0200

    refactor Signed-off-by:delfi <delfi@segonquart.localhost>

--- v03/nastymondays/404.php
+++ v03/nastymondays/404.php
@@ -1,23 +1,32 @@
 <?php
 /*
-* Template Name: 404
+ * Template Name: 404
  *
  * @package WordPress
  * @subpackage nastymondays
  */
-get_header();
+get_header ();
 ?>
-<body <?php body_class(); ?>>
-<div id="wrapper">
-<div id="pasador_login">
-       <h1 class="cuatro0cuatro">
-       <a href="/">
-       <img src="src/images/err/404.jpg" 
-       alt="<?php print "Error 404. Nasty Mondays unsuccessful try. We can't seem to find what you're looking for. Let us take you back home.", ?> />
-       </a>
-       </h1>
-       <?php include (TEMPLATEPATH . "modules/searchform.php"); ?>
-</div>
-</div>
+
+<body <?php body_class (); ?>>
+    <div id="wrapper">
+
+        <div id="pasador_login">
+
+            <h1 class="cuatro0cuatro">
+
+                <a href="/">
+
+                    <img src="src/images/err/404.jpg"
+                         alt="<?php print 'Error 404. Nasty Mondays unsuccessful try. We cabbot seem to find what you are looking for. Let us take you back home.'; ?>"
+                </a>
+            </h1>
+
+            <?php include (TEMPLATEPATH . "modules/searchform.php"); ?>
+
+        </div>
+
+    </div>
+
 </body>
 </html>
