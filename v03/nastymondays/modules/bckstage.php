3c22a11b5b0605ecb6dbc1551bfa1782d403983b
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-06-22 16:18:52 +0200

    init commit

---
+++ v03/nastymondays/modules/bckstage.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Custom Module BckStage
+ *
+ * @package WordPress
+ * @subpackage nastymondays
+ */
+ 
+ $urlObject ="http://nastymondays.com/backstage/";
+?>
+<h4 class="backstage">
+<a href="<?php echo $urlObject; ?>">
+<?php print " &spades;  &spades;  &spades;  Visit Nasty Mondays Backstage. Why don't you stay in the bar that night They got your numbers. Social Security numbers. Everybody's fuckin' numbers. It's free and you'll meet Soren and Max.When you decide to be something, you can be it. That's what they don't tell you in the church.  &spades;  &spades;  &spades; " ?>
+</a>
+</h4>
