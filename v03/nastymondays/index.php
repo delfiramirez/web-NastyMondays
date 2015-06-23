266ea065258146586c0241234a497aab6e29ec04
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-06-23 15:15:58 +0200

    init commit

---
+++ v03/nastymondays/index.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * Template Name: Page
+ *
+ * @package WordPress
+ * @subpackage nastymondays
+ */
+?>
+<div id="pasador8"></div>
+
+ <?php query_posts($query_string . '&cat=-126,-127'); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="post">
+<div class="post-date">
+<span class="post-day">
+<?php the_time('j') ?>
+</span>
+<span class="post-month">
+<?php the_time('M') ?>
+</span> 
+</div>
+
+<span class="post-cat">
+<?php echo my_entry_published_link(); ?>
+</span>
+
+<div class="post-title">
+<h2>
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+<?php the_title(); ?>
+</a>
+</h2>
+</div>
+
+<span class="post-cat">
+<?php the_category(' : ') ?>
+</span>
+
+<div class="entry">
+
+<?php if ( is_page() && ($post==$posts[0] || $post==$posts[1] || $post==$posts[2]) && !is_paged() ) : ?> 
+<?php the_content(); ?> 
+<?php else: ?>
+<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+<?php the_post_thumbnail('featured-thumbnail'); ?>
+</a> 
+<?php the_excerpt(); ?>
+<p class="nasty">
+<a href="<?php the_permalink() ?>">
+<?php print 'Read more &raquo;'; ?>
+</a>
+</p>
+<?php endif; ?>
+</div>
+<?php endwhile; ?>
+
+<div class="pagenavi"> 
+<?php 
+if(function_exists('wp_paginate')) 
+{ 
+wp_paginate('range=9&anchor=2&nextpage=Next&previouspage=Previous'); 
+}
+?>
+</div>
+	<?php  else: ?>
+<?php get_template_part( 'modules/nofound' );  ?>
+	<?php endif; ?>
+</div>
+
+<hr />
+<?php get_template_part( 'modules/bckstage' );  ?>
+<?php wp_nav_menu('menu=NastyMondays'); ?>	            
+<?php get_template_part( 'modules/segonquart' );  ?>
+
+<?php get_footer(); ?>
+</div>	
+</body>
+</html>
---
+++ v03/nastymondays/modules/blah.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Template Part: Contact Blah
+ *
+ * @package WordPress
+ * @subpackage nastymondays
+ */
+
+<h2 class="nasty2">Contact with $blogName </h2>
+<p> &spades; A location in touch with the personality of the project, between the line of a bohemian attitude and the modernity of Barcelona city; characterized by its multi-cultural, innovation, tradition and creativity together with its own identity.</p>
+<p> &spades; Do you have any crazy idea for your $blogName?.. You ever want to earn a little extra money, you come by L street. You know where I am on L street.
+<br />Please, fill the form, and we will CONTACT YOU.
+</p>
