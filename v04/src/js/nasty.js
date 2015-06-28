fbd5b514f90dc1d933d06d0abc4e30aa24f60004
delfi <delfi@segonquart.localhost>
2015-06-28 06:46:08 +0200

    js files Signed-off-by:delfi <delfi@segonquart.localhost>

--- v04/src/js/nasty.js
+++ v04/src/js/nasty.js
@@ -1,1 +1,18 @@
-document.execCommand("BackgroundImageCache",false,true);
+document.execCommand ("BackgroundImageCache",false,true);
+
+function detectBrowser ()
+    {
+        var useragent = navigator.userAgent;
+        var mapdiv = document.getElementById ("map_canvas");
+
+        if (useragent.indexOf ('iPhone') != - 1 || useragent.indexOf ('Android') != - 1)
+            {
+                mapdiv.style.width = '100%';
+                mapdiv.style.height = '100%';
+            }
+        else
+            {
+                mapdiv.style.width = '389px';
+                mapdiv.style.height = '390px';
+            }
+    }
