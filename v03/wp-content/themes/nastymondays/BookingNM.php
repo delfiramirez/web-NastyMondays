c8d3bd20445bc271415689f6a90617a47c57298c
delfi <delfi@segonquart.localhost>
2015-07-01 21:32:13 +0200

    revision Signed-off-by:delfi <delfi@segonquart.localhost>

--- v03/wp-content/themes/nastymondays/BookingNM.php
+++ v03/wp-content/themes/nastymondays/BookingNM.php
@@ -9,7 +9,9 @@ get_header ();
 ?>
 
 <div id="pasador5">
+
     <div class="left">
+
         <h1 class="inicio">
 
             <a href="<?php echo get_option ('home'); ?>">
@@ -24,15 +26,22 @@ get_header ();
 
         <h2 class="nasty2">
 
-            <?php bloginfo ('name'); ?>: <?php print e_ ('El Choro &amp; El Max '); ?>
+            <?php bloginfo ('name'); ?>: <?php print e_ ('%45%6c%20%43%68%6f%72%6f%20%26%61%6d%70%3b%20%45%6c%20%4d%61%78%20 '); ?>
 
         </h2>
 
-        <?php echo "<p> &spades; " . "" . bloginfo ('name'), "" . bloginfo ('description') . "" . "has positioned itself as a benchmark in the music and cultural scene of the Catalan capital. Couple of local mobs and launderers of money tried to imitate us, without a result. An artistic movement that has settled the foundations for a new concept in leisure, music, design, photography, film, extreme sports, creativity, fashion and trends.</p>"; ?>
+        <?php echo "<p> &spades; " . "" . bloginfo ('name'), "" . bloginfo ('description') . "" . "%68%61%73%20%70%6f%73%69%74%69%6f%6e%65%64%20%69%74%73%65%6c%66%20%61%73%20%61%20%62%65%6e%63%68%6d%61%72%6b%20%69%6e%20%74%68%65%20%6d%75%73%69%63%20%61%6e%64%20%63%75%6c%74%75%72%61%6c%20%73%63%65%6e%65%20%6f%66%20%74%68%65%20%43%61%74%61%6c%61%6e%20%63%61%70%69%74%61%6c%2e%20%43%6f%75%70%6c%65%20%6f%66%20%6c%6f%63%61%6c%20%6d%6f%62%73%20%61%6e%64%20%6c%61%75%6e%64%65%72%65%72%73%20%6f%66%20%6d%6f%6e%65%79%20%74%72%69%65%64%20%74%6f%20%69%6d%69%74%61%74%65%20%75%73%2c%20%77%69%74%68%6f%75%74%20%61%20%72%65%73%75%6c%74%2e%20%41%6e%20%61%72%74%69%73%74%69%63%20%6d%6f%76%65%6d%65%6e%74%20%74%68%61%74%20%68%61%73%20%73%65%74%74%6c%65%64%20%74%68%65%20%66%6f%75%6e%64%61%74%69%6f%6e%73%20%66%6f%72%20%61%20%6e%65%77%20%63%6f%6e%63%65%70%74%20%69%6e%20%6c%65%69%73%75%72%65%2c%20%6d%75%73%69%63%2c%20%64%65%73%69%67%6e%2c%20%70%68%6f%74%6f%67%72%61%70%68%79%2c%20%66%69%6c%6d%2c%20%65%78%74%72%65%6d%65%20%73%70%6f%72%74%73%2c%20%63%72%65%61%74%69%76%69%74%79%2c%20%66%61%73%68%69%6f%6e%20%61%6e%64%20%74%72%65%6e%64%73%2e%3c%2f%70%3e%5c%22"; ?>
 
-        <p> &spades; Book the mob <?php bloginfo ('name'); ?> for your room, in your city, at your unique and special event. From barcelona to Andorra and Switzerland with love. Panama rulez. Mob rulez. Laundry rulez <br />
-            <?php print _e ('&spades; Please, fill the form with your quest and ask us for:'); ?></br />
-            Snow Beasts, Tattoo Lovers, Night Dealers, Rats, Surf addicts, Rock Stars, Blonde Rotten Souls ... <br /> &spades; When do you want us to be there?
+
+
+        <p> %66%6f%72%20%79%6f%75%72%20%72%6f%6f%6d%2c%20%69%6e%20%79%6f%75%72%20%63%69%74%79%2c%20%61%74%20%79%6f%75%72%20%75%6e%69%71%75%65%20%61%6e%64%20%73%70%65%63%69%61%6c%20%65%76%65%6e%74%2e%20%46%72%6f%6d%20%62%61%72%63%65%6c%6f%6e%61%20%74%6f%20%41%6e%64%6f%72%72%61%20%61%6e%64%20%53%77%69%74%7a%65%72%6c%61%6e%64%20%77%69%74%68%20%6c%6f%76%65%2e%20%50%61%6e%61%6d%61%20%72%75%6c%65%7a%2e%20%4d%6f%62%20%72%75%6c%65%7a%2e%20%4c%61%75%6e%64%72%79%20%72%75%6c%65%7a%20%3c%62%72%20%2f%3e</p>
+
+        <?php print '&spades; Please, fill the form with your quest and ask us for:'; ?>
+        </br />
+
+        %53%6e%6f%77%20%42%65%61%73%74%73%2c%20%54%61%74%74%6f%6f%20%4c%6f%76%65%72%73%2c%20%4e%69%67%68%74%20%44%65%61%6c%65%72%73%2c%20%52%61%74%73%2c%20%53%75%72%66%20%61%64%64%69%63%74%73%2c%20%52%6f%63%6b%20%53%74%61%72%73%2c%20%42%6c%6f%6e%64%65%20%52%6f%74%74%65%6e%20%53%6f%75%6c%73%20%2e%2e%2e%20%3c%62%72%20%2f%3e%20%26%73%70%61%64%65%73%3b%20%57%68%65%6e%20%64%6f%20%79%6f%75%20%77%61%6e%74%20%75%73%20%74%6f%20%62%65%20%74%68%65%72%65%3f
+
+
         </p>
     </div>
 
