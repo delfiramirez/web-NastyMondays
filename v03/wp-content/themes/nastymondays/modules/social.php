e6d0fc02f641e151bf731c6aafc05715b16a4883
Segonquart Studio {public repositories} <delfiramirez@users.noreply.github.com>
2015-07-28 01:20:33 +0200

    Update social.php

--- v03/wp-content/themes/nastymondays/modules/social.php
+++ v03/wp-content/themes/nastymondays/modules/social.php
@@ -10,6 +10,6 @@ get_header ();
 
 <div id="social">
 
-    <?php echo '&spades;  <a class ="nasty3" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank">On Facebook</a> &spades; <a class="nasty3" href="http://twitter.com/home?status=Currently reading <?php the_permalink(); ?>" title="on  Twitter!" target="_blank">on Twitter</a> &spades;'; ?>
+    <?php echo (htmlentities('&spades;'). '<a class ="nasty3" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank">On Facebook</a> &spades; <a class="nasty3" href="http://twitter.com/home?status=Currently reading <?php the_permalink(); ?>" title="on  Twitter!" target="_blank">on Twitter</a>)'. htmlentities('&spades;')); ?>
 
 </div>
