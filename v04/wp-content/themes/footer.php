fda3cbcbd0cbad4958d8273b9dc4ea50767ef049
Segonquart Studio {public repositories} <delfiramirez@users.noreply.github.com>
2015-07-28 01:33:24 +0200

    Update footer.php

--- v04/wp-content/themes/footer.php
+++ v04/wp-content/themes/footer.php
@@ -20,10 +20,10 @@
             </li>
             <li class="legal">
 
-                Official Photos by <a href="http://carpiogm.blogspot.com/">%4d%72%2e%20%4d%61%72%69%6f%20%43%61%72%70%69%6f </a>
+                Official Photos by <a href="http://carpiogm.blogspot.com/"> <?php echo hex2bin("%4d%72%2e%20%4d%61%72%69%6f%20%43%61%72%70%69%6f"); ?> </a>
             </li>
             <li class="legal">
-                Community Manager <a href="http://www.welcometoneko.com/p/about.html">%4e%65%6b%6f%20%4d%61%6e </a>
+                Community Manager <a href="http://www.welcometoneko.com/p/about.html"><?php echo hex2bin("%4e%65%6b%6f%20%4d%61%6e"); ?> </a>
             </li>
         </ul>
 
@@ -38,11 +38,11 @@
         <a href="<?php bloginfo ('url'); ?>">
             <?php bloginfo ('name'); ?>
         </a>
-    </strong> %77%61%73%20%69%6e%76%65%6e%74%65%64%20%35%20%79%65%61%72%73%20%61%67%6f%20%69%6e%20 <?php bloginfo ('description'); ?>, by our friends <strong><a href="http://greenselfstorage.es" >Max "GreenSelfStorage"</a></strong> and <strong>El Choro</strong> - local celebrities, mad minds, blonde lovers and tattoo addicts! With their fable to indie and electro rock, they started with a party series that did not exist like that in <strong>Barcelona</strong> before. Constantly reinventing themselves, with new themes, bands playing live, different locations and becoming crazier each time, they have managed to establish their base within Barcelona, and have toured in Denmark, Sweden, Germany, Austria, Holland or the United States.</p>
+    </strong> <?php echo hex2bin("%77%61%73%20%69%6e%76%65%6e%74%65%64%20%35%20%79%65%61%72%73%20%61%67%6f%20%69%6e%20"); ?> <?php bloginfo ('description'); ?>, <?php echo hex2bin("206279206f757220667269656e6473"); ?>. <?php echo ('<strong><a href="http://greenselfstorage.es">Max "GreenSelfStorage"</a></strong>'. htmlentities ('&'). '<strong>') ; ?><?php echo hex2bin("456c2043686f726f"); ?> </strong> <?php echo hex2bin("2d206c6f63616c2063656c65627269746965732c206d6164206d696e64732c20626c6f6e6465206c6f7665727320616e6420746174746f6f2061646469637473212057697468207468656972206661626c6520746f20696e64696520616e6420656c656374726f20726f636b2c207468657920737461727465642077697468206120706172747920736572696573207468617420646964206e6f74206578697374206c696b65207468617420696e203c7374726f6e673e42617263656c6f6e613c2f7374726f6e673e206265666f72652e20436f6e7374616e746c79207265696e76656e74696e67207468656d73656c7665732c2077697468206e6577207468656d65732c2062616e647320706c6179696e67206c6976652c20646966666572656e74206c6f636174696f6e7320616e64206265636f6d696e67206372617a69657220656163682074696d652c20746865792068617665206d616e6167656420746f2065737461626c69736820746865697220626173652077697468696e2042617263656c6f6e612c20616e64206861766520746f7572656420696e2044656e6d61726b2c2053776564656e2c204765726d616e792c20417573747269612c20486f6c6c616e64206f722074686520556e6974656420537461746573"). '</p>' ; ?>
 
-<p>%49%74%20%61%70%70%65%61%72%73%20%74%68%61%74%20%4a%61%76%61%53%63%72%69%70%74%20%69%73%20%64%69%73%61%62%6c%65%64%20%69%6e%20%79%6f%75%72%20%62%72%6f%77%73%65%72%2c%20%6f%72%20%79%6f%75%72%20%62%72%6f%77%73%65%72%20%64%6f%65%73%6e%5c%27%74%20%73%75%70%70%6f%72%74%20%69%74%2e%20%54%6f%20%65%6e%6a%6f%79
+<p><?php echo hex2bin("%49%74%20%61%70%70%65%61%72%73%20%74%68%61%74%20%4a%61%76%61%53%63%72%69%70%74%20%69%73%20%64%69%73%61%62%6c%65%64%20%69%6e%20%79%6f%75%72%20%62%72%6f%77%73%65%72%2c%20%6f%72%20%79%6f%75%72%20%62%72%6f%77%73%65%72%20%64%6f%65%73%6e%5c%27%74%20%73%75%70%70%6f%72%74%20%69%74%2e%20%54%6f%20%65%6e%6a%6f%79"); ?>
 
-    <?php the_title (); ?>%20%6c%6f%67%20%69%6e%20%79%6f%75%72%20%61%63%63%6f%75%6e%74%20%61%6e%64%20%68%61%76%61%20%61%20%66%75%6c%6c%20%62%72%6f%77%73%69%6e%67%20%65%78%70%65%72%69%65%6e%63%65%2c%20%79%6f%75%5c%27%6c%6c%20%6e%65%65%64%20%74%6f%20%61%63%74%69%76%61%74%65%20%4a%61%76%61%53%63%72%69%70%74
+    <?php the_title (); ?><?php echo hex2bin("%20%6c%6f%67%20%69%6e%20%79%6f%75%72%20%61%63%63%6f%75%6e%74%20%61%6e%64%20%68%61%76%61%20%61%20%66%75%6c%6c%20%62%72%6f%77%73%69%6e%67%20%65%78%70%65%72%69%65%6e%63%65%2c%20%79%6f%75%5c%27%6c%6c%20%6e%65%65%64%20%74%6f%20%61%63%74%69%76%61%74%65%20%4a%61%76%61%53%63%72%69%70%74"); ?>
 
     .</p>
 </noscript>
